fd4eddf4715411a9629c9419de8ee868003fe8df
An Luu <atluutran@gmail.com>
2025-02-04 09:46:03 -0500

    Added GoogleSignIn package and logic.

---
+++ MyGoogleSignIn/MyGoogleSignInTests/MyGoogleSignInTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyGoogleSignInTests.swift
+//  MyGoogleSignInTests
+//
+//  Created by An Luu on 2/4/25.
+//
+
+import Testing
+@testable import MyGoogleSignIn
+
+struct MyGoogleSignInTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
